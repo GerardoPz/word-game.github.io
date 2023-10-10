3e918950258e35b5061fb551b646a550322dea01
Gerardo <pliegolugohectorgerardo@gmail.com>
2023-10-10 15:19:01 -0600

    CODIGO MAS LEIBLE

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Arreglo de palabras
 const palabras = [
     "sol",
     "luna",
@@ -9,178 +10,153 @@ const palabras = [
     "verde",
     "vino",
     "flor"
-];
+  ];
   
-// Función para obtener una palabra aleatoria del arreglo
-function obtenerPalabraAleatoria() {
+  // Función para obtener una palabra aleatoria del arreglo
+  function obtenerPalabraAleatoria() {
     // Genera un índice aleatorio dentro del rango del arreglo
     const indiceAleatorio = Math.floor(Math.random() * palabras.length);
-  
     // Devuelve la palabra correspondiente al índice aleatorio
     return palabras[indiceAleatorio];
-}
-
-function desordenarPalabra(palabra) {
+  }
+  
+  // Función para desordenar una palabra
+  function desordenarPalabra(palabra) {
     // Convierte la palabra en un arreglo de caracteres
     const arregloLetras = palabra.split('');
   
     // Utiliza el algoritmo de Fisher-Yates para mezclar las letras al azar
     for (let i = arregloLetras.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arregloLetras[i], arregloLetras[j]] = [arregloLetras[j], arregloLetras[i]];
+      const j = Math.floor(Math.random() * (i + 1));
+      [arregloLetras[i], arregloLetras[j]] = [arregloLetras[j], arregloLetras[i]];
     }
   
     // Convierte el arreglo de letras de nuevo en una palabra
     const palabraDesordenada = arregloLetras.join('');
     return palabraDesordenada;
-}
-
-let palabra = obtenerPalabraAleatoria()
-console.log(desordenarPalabra(palabra))
-
-const wordAnsInput = document.querySelector('.word-ans');
-
-palabraDesordenada = desordenarPalabra(palabra)
-
-wordAnsInput.setAttribute('value', palabraDesordenada);
-
-// Palabra a adivinar
-let word = palabra;
-let wordArray = word.split('');
-
-// Array para almacenar las letras ingresadas por el usuario
-let arrayUser = [];
-
-// Bandera para determinar si el juego ha sido ganado
-let won = false;
-
-// Variable que indica el numero de intentos disponibles
-const maxTries = 5;
-// Contador de intentos
-let intentos = 0;
-
-// Función para inicializar el juego
-function startGame() {
-    // Reiniciar variables
+  }
+  
+  // Inicialización de la palabra y elementos HTML
+  let palabra = obtenerPalabraAleatoria();
+  
+  const wordAnsInput = document.querySelector('.word-ans');
+  palabraDesordenada = desordenarPalabra(palabra);
+  wordAnsInput.setAttribute('value', palabraDesordenada);
+  
+  // Palabra a adivinar y variables del juego
+  let word = palabra;
+  let wordArray = word.split('');
+  let arrayUser = []; // Array para almacenar las letras ingresadas por el usuario
+  let won = false; // Bandera para determinar si el juego ha sido ganado
+  const maxTries = 5; // Número máximo de intentos
+  let intentos = 0; // Contador de intentos
+  
+  // Función para inicializar el juego
+  function startGame() {
     arrayUser = [];
     won = false;
     intentos = 0;
-
-    // Inicializar el primer row-wordg
     createRowWord();
-}
-
-// Función para crear un nuevo row-word y eliminar el anterior
-function createRowWord() {
-    // Seleccionar el contenedor donde se insertará el row-word
+  }
+  
+  // Función para crear un nuevo row-word y eliminar el anterior
+  function createRowWord() {
     const containerElement = document.getElementById('container-row-word');
-
-    // Elimina el row-word anterior, si existe dentro del contenedor
     let existingRowWord = containerElement.querySelector('.row-word');
     if (existingRowWord) {
-        existingRowWord.remove();
+      existingRowWord.remove();
     }
-
     let newRowWord = document.createElement('div');
     newRowWord.className = 'row-word';
     containerElement.appendChild(newRowWord);
     createInput(wordArray, newRowWord);
-}
-
-
-// Función para crear los cuadros de entrada
-function createInput(array, rowWord) {
+  }
+  
+  // Función para crear los cuadros de entrada
+  function createInput(array, rowWord) {
     rowWord.innerHTML = '';
     array.forEach(() => {
-        let input = document.createElement('input');
-        input.type = 'text';
-        input.maxLength = 1;
-        input.className = 'square';
-        rowWord.appendChild(input);
+      let input = document.createElement('input');
+      input.type = 'text';
+      input.maxLength = 1;
+      input.className = 'square';
+      rowWord.appendChild(input);
     });
     rowWord.firstChild.classList.add('focus');
-    // Establecer el enfoque en el primer input
     rowWord.firstChild.focus();
-}
-
-
-// Función para verificar la palabra ingresada por el usuario
-function checkWord() {
+  }
+  
+  // Función para verificar la palabra ingresada por el usuario
+  function checkWord() {
     intentos++;
     updateTriesDisplay();
     if (arrayUser.join('') !== word) {
-        if (intentos >= 5) {
-            paintCirclePurple(intentos);
-            document.getElementById("container-main").style.display = "none";
-            document.getElementById("msj-win-lose").style.display = "flex";
-            let winMessage = document.getElementById('win-lose')
-            let intentosElement = document.getElementById('intents');
-            winMessage.textContent = `PERDISTE`
-            intentosElement.textContent = `AGOTASTE TODOS LOS INTENTOS`;
-        } else {
-            // Pintamos el círculo para indicar que desperdiciamos un intento
-            paintCirclePurple(intentos);
-            arrayUser = [];
-            createRowWord(); // Crea un nuevo row-word para continuar adivinando
-        }
-    } else {
-        won = true;
+      if (intentos >= maxTries) {
+        paintCirclePurple(intentos);
         document.getElementById("container-main").style.display = "none";
         document.getElementById("msj-win-lose").style.display = "flex";
-        let winMessage = document.getElementById('win-lose')
+        let winMessage = document.getElementById('win-lose');
         let intentosElement = document.getElementById('intents');
-        winMessage.textContent = `GANASTE FELICIDADES`
-        intentosElement.textContent = `En ${intentos} Intentos`;
+        winMessage.textContent = `PERDISTE`;
+        intentosElement.textContent = `AGOTASTE TODOS LOS INTENTOS`;
+      } else {
+        paintCirclePurple(intentos);
+        arrayUser = [];
+        createRowWord();
+      }
+    } else {
+      won = true;
+      document.getElementById("container-main").style.display = "none";
+      document.getElementById("msj-win-lose").style.display = "flex";
+      let winMessage = document.getElementById('win-lose');
+      let intentosElement = document.getElementById('intents');
+      winMessage.textContent = `GANASTE FELICIDADES`;
+      intentosElement.textContent = `En ${intentos} Intentos`;
     }
-}
-
-// Función para pintar el círculo correspondiente de morado
-function paintCirclePurple(attempts) {
+  }
+  
+  // Función para pintar el círculo correspondiente de morado
+  function paintCirclePurple(attempts) {
     const circles = document.querySelectorAll(".circle");
     if (attempts <= maxTries) {
-        circles[attempts - 1].classList.add("circle-purple");
+      circles[attempts - 1].classList.add("circle-purple");
     }
-}
-
-function updateTriesDisplay() {
+  }
+  
+  // Función para actualizar la visualización de intentos
+  function updateTriesDisplay() {
     document.querySelector(".container-tries span").textContent = `Intentos (${intentos}/${maxTries}):`;
-}
-
-// Evento para detectar la entrada de texto del usuario
-document.addEventListener('input', (event) => {
-
+  }
+  
+  // Evento para detectar la entrada de texto del usuario
+  document.addEventListener('input', (event) => {
     if (!won && event.target.classList.contains('square')) {
-        arrayUser.push(event.target.value.toLowerCase());
-        if (event.target.nextElementSibling) {
-            event.target.nextElementSibling.focus();
-        } else {
-            checkWord(); // Verificar la palabra cuando se completen todos los campos
-        }
+      arrayUser.push(event.target.value.toLowerCase());
+      if (event.target.nextElementSibling) {
+        event.target.nextElementSibling.focus();
+      } else {
+        checkWord();
+      }
     }
-});
-
-
-// Obtén una referencia al botón "Reset" por su ID
-const resetButton = document.getElementById("reset");
-
-// Agrega un evento clic al botón "Reset"
-resetButton.addEventListener("click", () => {
-  // Reinicia los intentos y el juego sin cambiar la palabra
-  intentos = 0;
-  updateTriesDisplay();
-  createRowWord();
-  
-  // Agrega la clase circle-gray a todos los círculos
-  const circles = document.querySelectorAll(".circle");
-  circles.forEach(circle => {
-    circle.classList.add("circle-gray");
   });
   
-  // Quita la clase circle-purple de todos los círculos
-  circles.forEach(circle => {
-    circle.classList.remove("circle-purple");
+  // Obtén una referencia al botón "Reset" por su ID
+  const resetButton = document.getElementById("reset");
+  
+  // Agrega un evento clic al botón "Reset"
+  resetButton.addEventListener("click", () => {
+    intentos = 0;
+    updateTriesDisplay();
+    createRowWord();
+    const circles = document.querySelectorAll(".circle");
+    circles.forEach(circle => {
+      circle.classList.add("circle-gray");
+    });
+    circles.forEach(circle => {
+      circle.classList.remove("circle-purple");
+    });
   });
-});
-
-// Inicializa el juego cuando se carga la página
-startGame();
+  
+  // Inicializa el juego cuando se carga la página
+  startGame();
+  
